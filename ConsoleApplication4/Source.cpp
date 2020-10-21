2edf34faceefe335ef3975b91e0505c6c4fbe674
Tropria <NeoTroria@gmail.com>
2020-10-21 12:18:23 +0800

    Try to upload a head file

--- ConsoleApplication4/Source.cpp
+++ ConsoleApplication4/Source.cpp
@@ -1,5 +1,15 @@
 
 #include <iostream>
+// #墙壁 _空间 .目的地 o箱子 p玩家
+const char gStageData[] = "\
+########\n\
+# .. p #\n\
+# oo   #\n\
+#      #\n\
+########";
+const int gStageHeight = 5;
+const int gStageWidth = 8;
+
 using namespace std;
 int main() {
 	cout<<"new myBox"<<endl;
